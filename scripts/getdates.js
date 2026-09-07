c7b3e41bda1931f686cc2b42517360db2e978ee2
Heriniaina <andriananjamarotafika@gmail.com>
2026-09-07 22:48:47 +0300

    w01 Done

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+let date = new Date();
+let year = date.getFullYear();
+
+document.getElementById("current-year").textContent = year;
+document.getElementById("lastModified").textContent = document.lastModified;
